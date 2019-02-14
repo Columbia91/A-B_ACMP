a42116c936e9a05aed32848dc06ebf2b17061ace
cougar91 <alpha.z91@mail.ru>
2019-02-14 17:19:11 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\ADILET\source\repos\ConsoleApp20\ConsoleApp20\input.txt");
+            
+            string[] numbers = sr.ReadToEnd().Split();
+
+            StreamWriter sw = new StreamWriter(@"C:\Users\ADILET\source\repos\ConsoleApp20\ConsoleApp20\output.txt");
+
+            sw.WriteLine(int.Parse(numbers[0]) + int.Parse(numbers[1]));
+            sw.Close();
+        }
+    }
+}
